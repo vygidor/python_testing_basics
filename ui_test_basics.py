f9653e6c87c15ec36931fef02ef3e971a5642bd4
V <vygidor@users.noreply.github.com>
2015-06-11 15:11:38 +0200

    update ui_test_basics.py enhanced and commented - still not working right ui_test_basics.py used as example of working search with input

--- ui_test_basics.py
+++ ui_test_basics.py
@@ -3,23 +3,37 @@ __author__ = 'vygidor'
 import unittest
 from selenium import webdriver
 
+
 class UITest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-         #test will be performed in Firefox
+        # test will be performed in Firefox
         cls.driver = webdriver.Firefox()
 
-#open page in browser
-    def test_title(google):
-        google.driver.get('http://www.google.com')
-        google.assertEqual(
-            google.driver.title,
+# open page in browser
+    def test_title(self):
+        self.driver.get('https://www.google.com')
+        self.assertEqual(
+            self.driver.title,
             'Google')
 
-#here comes the magic :)
-#testing push from pycharm
+# here comes the magic :)
 
-#close the browser window
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+# fill search field
+    def text_search(self):
+        # google.driver.get('http://www.google.com')
+        search_input = self.driver.find_element_by_name("q")
+        search_input.send_keys("Python Selenium test driven testing")
+        search_input.submit()
+
+        print self.driver.title
+
+# hit the button
+        # google.driver.get('http://www.google.com')
+        # find_button = self.driver.find_element_by_name("btnK")
+        # find_button.click()
+
+# close the browser window
+#    @classmethod
+#    def tearDownClass(cls):
+#        cls.driver.quit()
