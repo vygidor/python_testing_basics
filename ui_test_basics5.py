7cf3179357e5a3f9fff8e871032c4cbb7ffe196b
vygidor <vygidor@gmail.com>
2016-01-12 20:15:00 +0100

    improved ui_test_basic5 (wait until element is clickable)

--- ui_test_basics5.py
+++ ui_test_basics5.py
@@ -3,19 +3,18 @@ __author__ = 'vygidor'
 import unittest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 
 class UITest(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        #test will be performed in Chrome
+        # test will be performed in Chrome
         cls.driver = webdriver.Chrome()
 
-    #open page in Chrome
+    # open page in Chrome
     def test_1_title(self):
         self.driver.get('http://www.google.com')
         self.assertEqual(
@@ -32,27 +31,36 @@ class UITest(unittest.TestCase):
     # the same as test_2_searchbyname - the element will be defined by XPATH
     def test_3_search_by_xpath(self):
         self.driver.get('http://www.google.com')
-        #search_input = self.driver.find_element_by_xpath('//*[@id="q"]') //*[@id="lst-ib"]
+        # search_input = self.driver.find_element_by_xpath('//*[@id="q"]') //*[@id="lst-ib"]
         search_input = self.driver.find_element_by_xpath('//*[@id="lst-ib"]')
         search_input.send_keys("XPath")
         search_input.submit()
 
     # will wait until the element is loaded but not longer as defined in variable
     # Note: you have to extend your imports:
-    #       from selenium.webdriver.support.ui import WebDriverWait
+    # from selenium.webdriver.support.ui import WebDriverWait
     #       from selenium.webdriver.support import expected_conditions as EC
     #       from selenium.webdriver.common.by import By
     #       from selenium.common.exceptions import TimeoutException
     def test_4_wait_until_loaded(self):
         self.driver.get('http://www.google.com')
-        delay = 20 # seconds to wait for the element to appear
+        # seconds to wait for the element to appear
+        delay = 20
         try:
-            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.ID, 'hplogo')))
+            WebDriverWait(self.driver, delay).until(ec.presence_of_element_located((By.ID, 'hplogo')))
             print "Loading successful!"
         except TimeoutException:
             print "Loading took too much time!"
 
-     # close the browser window
+    # will wait until the element is clickable (available to click) but not longer as defined in WebDriverWait (for this example it is 20 seconds)
+    def test_5_wait_until_clickable(self):
+        self.driver.get('http://toolsqa.com')
+        try:
+            WebDriverWait(self.driver, 20).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="main-nav"]/li[2]/a')))
+        except TimeoutException:
+            print "'Tutorials' button not available!"
+
+    # close the browser window
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
